6ec547fdcdabbe9052c681b4a7d58059d8c4b979
SalguJam <afterblow23@gmail.com>
2025-02-11 01:00:02 +0900

    hw: 0210

---
+++ 0210_1.js
@@ -0,0 +1,9 @@
+let a = 1
+let b = 2 
+console.log(a,b) // 1,2 
+
+/* 코드 후에*/ 
+a = 2
+b = 1
+
+console.log(a,b) //  2,1 
---
+++ 0210_2.js
@@ -0,0 +1,50 @@
+// first step
+let a = 30
+let b = 50
+let Temp = 0
+
+console.log(a,b,Temp) // 30,50,0
+
+// second step
+a = 0
+Temp = 30
+
+console.log(a,b,Temp) // 0,50,30
+
+// third step
+a = 50
+b = 0
+Temp = 30
+
+console.log(a,b,Temp) // 50,0,30
+
+// fourth step
+b = 30
+Temp = 0
+
+console.log(a,b,Temp) // 50,30,0
+
+
+20 + 30  // 50
+“20” + “30”
+“Hello” + " " + 2021
+1 + 2 * 3  // 7
+(1 + 3) ** 2  // 16
+1 / 0
+6 % 2  // 0
+7.5 % 2 // 1.5
+5 == 5  // true
+5 === 5  // true
+5 == “5”
+5 === “5”
+5 != 5.0  // false
+5 !== 5.0  // false
+“true” === true
+5 <= 5.0 // true
+5 >= 5 // true
+true || true  // true
+true || false  // true
+true && true  // true
+true && false  // false
+!true  // false
+!false // true
